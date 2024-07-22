6dbafd7e4f2e04bbf26b2486a8845f273ba357ac
Fariz Aotearoa Rasyid <faotearoarasyid@gmail.com>
2024-07-22 09:57:36 +0700

    Add type and attribute to response of get digimon list

--- backend/dto/digimon_list.go
+++ backend/dto/digimon_list.go
@@ -1,9 +1,11 @@
 package dto
 
 type DigimonList struct {
-	Number int    `gorm:"column:number" json:"number"`
-	Name   string `gorm:"column:name" json:"name"`
-	Stage  string `gorm:"column:stage" json:"stage"`
-	Image  string `gorm:"column:image" json:"image"`
-	Icon   string `gorm:"column:icon" json:"icon"`
+	Number    int    `gorm:"column:number" json:"number"`
+	Name      string `gorm:"column:name" json:"name"`
+	Stage     string `gorm:"column:stage" json:"stage"`
+	Type      string `gorm:"column:type" json:"type"`
+	Attribute string `gorm:"column:attribute" json:"attribute"`
+	Image     string `gorm:"column:image" json:"image"`
+	Icon      string `gorm:"column:icon" json:"icon"`
 }
--- backend/services/digimon_list_service.go
+++ backend/services/digimon_list_service.go
@@ -8,7 +8,7 @@ import (
 
 func GetAllDigimonList() ([]dto.DigimonList, error) {
 	var digimon []dto.DigimonList
-	query := `SELECT dd.number, dd.name, dd.stage, dd.image, dd.icon FROM digimon_details dd ORDER BY dd.number`
+	query := `SELECT dd.number, dd.name, dd.stage, dd.type, dd.attribute, dd.image, dd.icon FROM digimon_details dd ORDER BY dd.number`
 	if err := utils.DB.Raw(query).Scan(&digimon).Error; err != nil {
 		utils.Logger.Error("Failed to GetAllDigimonList", zap.Error(err))
 		return nil, err
